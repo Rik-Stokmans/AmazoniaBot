81b2356f97247b8885f20a996d67c76c14d83a22
Rik-Stokmans <71502476+Rik-Stokmans@users.noreply.github.com>
2024-09-08 21:43:20 +0200

    changed spacing

--- LogicLayer/Models/User.cs
+++ LogicLayer/Models/User.cs
@@ -2,8 +2,8 @@ namespace LogicLayer.Models;
 
 public abstract class User(ulong discordId, string minecraftUuid, string username, string password)
 {
-     public ulong DiscordId { get; set; } = discordId;
-     public string MinecraftUuid { get; set; } = minecraftUuid;
-     public string Username { get; set; } = username;
-     public string Password { get; set; } = password;
+    public ulong DiscordId { get; set; } = discordId;
+    public string MinecraftUuid { get; set; } = minecraftUuid;
+    public string Username { get; set; } = username;
+    public string Password { get; set; } = password;
 }
